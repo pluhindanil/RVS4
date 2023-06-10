c2ac8f9e8767e9a5918874d853287c739c0179e5
pluhindanil <134681522+pluhindanil@users.noreply.github.com>
2023-06-10 12:45:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,108 +1,78 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exhibitions.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///components.db'
 db = SQLAlchemy(app)
 api = Api(app)
 
-exhibition_model = api.model('Exhibition', {
-    'id': fields.Integer(readonly=True, description='The unique identifier of an exhibition'),
-    'name': fields.String(required=True, description='The name of the exhibition'),
-    'date': fields.String(required=True, description='The date of the exhibition'),
-    'location': fields.String(required=True, description='The location of the exhibition'),
-    'participants': fields.Integer(required=True, description='The number of participants')
+component_model = api.model('Component', {
+    'id': fields.Integer(readonly=True, description='The unique identifier of a component'),
+    'name': fields.String(required=True, description='The name of the component'),
+    'category': fields.String(required=True, description='The category of the component'),
+    'price': fields.Float(required=True, description='The price of the component')
 })
 
-participants_model = api.model('Participants', {
-    'participants': fields.Integer(required=True, description='The number of participants to add')
-})
-
-
-class Exhibition(db.Model):
+class Component(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    date = db.Column(db.String(80), nullable=False)
-    location = db.Column(db.String(120), nullable=False)
-    participants = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
-
-@api.route('/exhibitions')
-class ExhibitionResource(Resource):
-    @api.doc('list_exhibitions')
-    @api.marshal_list_with(exhibition_model)
+@api.route('/components')
+class ComponentResource(Resource):
+    @api.doc('list_components')
+    @api.marshal_list_with(component_model)
     def get(self):
-        '''List all exhibitions'''
-        exhibitions = Exhibition.query.all()
-        return exhibitions
+        '''List all components'''
+        components = Component.query.all()
+        return components
 
-    @api.doc('create_exhibition')
-    @api.expect(exhibition_model)
-    @api.marshal_with(exhibition_model, code=201)
+    @api.doc('create_component')
+    @api.expect(component_model)
+    @api.marshal_with(component_model, code=201)
     def post(self):
-        '''Create a new exhibition'''
+        '''Create a new component'''
         name = api.payload.get('name')
-        date = api.payload.get('date')
-        location = api.payload.get('location')
-        participants = api.payload.get('participants')
+        category = api.payload.get('category')
+        price = api.payload.get('price')
 
-        exhibition = Exhibition(name=name, date=date, location=location, participants=participants)
-        db.session.add(exhibition)
+        component = Component(name=name, category=category, price=price)
+        db.session.add(component)
         db.session.commit()
 
-        return exhibition, 201
+        return component, 201
 
-
-@api.route('/exhibitions/<int:id>')
-@api.param('id', 'The exhibition identifier')
-class ExhibitionItemResource(Resource):
-    @api.doc('update_exhibition')
-    @api.expect(exhibition_model)
-    @api.marshal_with(exhibition_model)
+@api.route('/components/<int:id>')
+@api.param('id', 'The component identifier')
+class ComponentItemResource(Resource):
+    @api.doc('update_component')
+    @api.expect(component_model)
+    @api.marshal_with(component_model)
     def put(self, id):
-        '''Update an exhibition'''
-        exhibition = Exhibition.query.get(id)
-        if exhibition:
-            exhibition.name = api.payload.get('name')
-            exhibition.date = api.payload.get('date')
-            exhibition.location = api.payload.get('location')
-            exhibition.participants = api.payload.get('participants')
+        '''Update a component'''
+        component = Component.query.get(id)
+        if component:
+            component.name = api.payload.get('name')
+            component.category = api.payload.get('category')
+            component.price = api.payload.get('price')
             db.session.commit()
-            return exhibition
+            return component
         else:
-            return {'message': 'Exhibition not found'}, 404
+            return {'message': 'Component not found'}, 404
 
-    @api.doc('delete_exhibition')
-    @api.response(204, 'Exhibition deleted')
+    @api.doc('delete_component')
+    @api.response(204, 'Component deleted')
     def delete(self, id):
-        '''Delete an exhibition'''
-        exhibition = Exhibition.query.get(id)
-        if exhibition:
-            db.session.delete(exhibition)
+        '''Delete a component'''
+        component = Component.query.get(id)
+        if component:
+            db.session.delete(component)
             db.session.commit()
             return '', 204
         else:
-            return {'message': 'Exhibition not found'}, 404
-
-
-# @api.route('/exhibitions/<int:id>/participants')
-# @api.param('id', 'The exhibition identifier')
-# class ExhibitionParticipantsResource(Resource):
-#     @api.doc('update_participants')
-#     @api.expect(participants_model)
-#     @api.marshal_with(exhibition_model)
-#     def patch(self, id):
-#         '''Update the participants of an exhibition'''
-#         participants_to_add = api.payload.get('participants')
-#
-#         exhibition = Exhibition.query.get(id)
-#         if exhibition:
-#             exhibition.participants += participants_to_add
-#             db.session.commit()
-#             return exhibition
-#         else:
-#             return {'message': 'Exhibition not found'}, 404
+            return {'message': 'Component not found'}, 404
 
 if __name__ == '__main__':
     with app.app_context():
